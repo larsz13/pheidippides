6e107755dae9c984d53fb7b50fa8ee5b498a89cb
larsz13 <104058308+larsz13@users.noreply.github.com>
2022-12-16 15:04:29 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+// Get the form data
+const form = document.querySelector('form');
+const formData = new FormData(form);
+
+// Convert the form data to a JSON object
+const data = {};
+formData.forEach((value, key) => {
+  data[key] = value;
+});
+const json = JSON.stringify(data);
+
+// Send the request to the API
+fetch('/api/endpoint', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: json
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
