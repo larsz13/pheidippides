b110cc6dec850ad3701f62d790da6de84c1b8b38
JanoschBuechi <91537661+JanoschBuechi@users.noreply.github.com>
2022-12-21 11:06:52 +0100

    deleting unecessary stuff

--- TwitterProject/twitterClient.js
+++ TwitterProject/twitterClient.js
@@ -7,18 +7,6 @@ const client = new TwitterApi({
     accessToken: "1600412941244932101-XyIfrvrlFkvyu1mslZc71U0J0z1fHs",
     accessSecret: "HdUzUEKMJDnQ5v8zRzQ12nTaAwdIbtiIO6LyJEKlovSCM"
 
-    /*
-    appKey: "wEDy945PY35nZX3sg9SgM70fz",
-    appSecret: "wUrrKN6scQQry1HO8cThX0lzz5hS958sI6P8kHrLJhgnJEbJxI",
-    accessToken: "1600412941244932101-HuEabsoQR5bwZTenFw7GlTfXnucSJy",
-    accessSecret: "J7dXV3QtY5L1vd3UGDot4nKJsBBlNDHS56LF1w4m6SXZc"
-    
-    consumer_key: "wEDy945PY35nZX3sg9SgM70fz",
-    consumer_secret: "wUrrKN6scQQry1HO8cThX0lzz5hS958sI6P8kHrLJhgnJEbJxI",
-    access_token_key: "1600412941244932101-HuEabsoQR5bwZTenFw7GlTfXnucSJy",
-    access_token_secret: "J7dXV3QtY5L1vd3UGDot4nKJsBBlNDHS56LF1w4m6SXZc"
-    */
-
 })
 
 const rwClient = client.readWrite
